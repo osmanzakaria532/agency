ac051d9981faf5d6629f668703cab2d569600220
osmanzakaria532 <osmanzakaria179@gmail.com>
2023-07-27 21:30:41 +0600

    Agency Website is complate

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.banner_slider_area').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true
+});
+
+
+$('.review_slider_part').slick({
+    slidesToShow: 3,
+    arrows: true,
+    prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
+    nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>'
+});
