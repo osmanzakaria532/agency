09107194bdf721b19cd272bed7498b120762a4ac
osmanzakaria532 <osmanzakaria179@gmail.com>
2023-08-06 22:48:48 +0600

    Extra Small Device is Complate

--- js/script.js
+++ js/script.js
@@ -17,21 +17,11 @@ $('.review_slider_part').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 767,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
